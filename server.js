9beb1a8a018770cfec22962e3f4fcf729cd40730
Lucas Duarte <lucashdp@outlook.com>
2019-01-06 18:06:37 -0200

    fix: fix the connection mongodb bug

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 MongoClient.connect(db.url, (err, database) => {
     if (err) return console.log(err)
-    routes(app, database);
+    routes(app, database.db("catalog"));
     app.listen(port, () => {
         console.log('We are live on ' + port);
     });
